757d97d630d40cb75854616d81e8736a1a288634
Yuri Dirickson <ydirickson@gmail.com>
2020-03-10 09:53:06 -0300

    Preparação para a aula 11 Autenticação

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+
+    nome = models.CharField(max_length=120)
+    sigla = models.CharField(max_length=5, blank=True)
+    duracao = models.SmallIntegerField()
+    descricao = models.TextField()
+
+class Disciplina(models.Model):
+
+    nome = models.CharField(max_length=150)
+    carga_horaria = models.IntegerField()
+
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING)
